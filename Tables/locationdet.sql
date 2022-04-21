90e26ee8a6d4d5cf736f02737249dda73d296a0e
AKS <93655638+AKS2010@users.noreply.github.com>
2022-04-21 12:25:48 +0530

    Tables Folder Creation
    Tables Folder Creation

---
+++ Tables/locationdet.sql
@@ -0,0 +1,14 @@
+Create table locationdet
+(
+locationcode	smallint		,
+region			varchar(250)	,
+country			varchar(250)	,
+state			varchar(250)	,
+district		varchar(250)	,
+city			varchar(250)	,
+village			varchar(250)	,
+createdby		varchar(150)	,
+createddate		smalldatetime	,
+modifiedby		varchar(150)	,
+modifieddate	smalldatetime
+)
