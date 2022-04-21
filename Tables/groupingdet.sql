e8c21030aab7b553de2e9cd40c925340679506fa
AKS <93655638+AKS2010@users.noreply.github.com>
2022-04-21 16:48:07 +0530

    AddingKeys
    Adding keys to all table

--- Tables/groupingdet.sql
+++ Tables/groupingdet.sql
@@ -1,12 +1,13 @@
 Create table groupingdet
 (
-groupcode		int				,
-groupid			varchar(100)	,
-groupname		varchar(256)	,
-locationcode	smallint		,
-fuelsource		varchar(256)	,
-createdby		varchar(150)	,
-createddate		smalldatetime	,
-modifiedby		varchar(150)	,
+groupcode		int				not null	,
+groupid			varchar(100)	not null	,
+groupname		varchar(256)				,
+locationcode	smallint					,
+fuelsource		varchar(256)				,
+createdby		varchar(150)				,
+createddate		smalldatetime				,
+modifiedby		varchar(150)				,
 modifieddate	smalldatetime
+primary key	(groupcode)
 )
