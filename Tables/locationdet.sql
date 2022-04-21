58a87a7c9a56369bada4f6a1b7de28c702517493
AKS <93655638+AKS2010@users.noreply.github.com>
2022-04-21 12:28:15 +0530

    Update Locationdetails
    Updating locationdet table with primary key

--- Tables/locationdet.sql
+++ Tables/locationdet.sql
@@ -1,14 +1,15 @@
 Create table locationdet
 (
-locationcode	smallint		,
-region			varchar(250)	,
-country			varchar(250)	,
-state			varchar(250)	,
-district		varchar(250)	,
-city			varchar(250)	,
-village			varchar(250)	,
-createdby		varchar(150)	,
-createddate		smalldatetime	,
-modifiedby		varchar(150)	,
+locationcode	smallint	not null	,
+region			varchar(250)			,
+country			varchar(250)			,
+state			varchar(250)			,
+district		varchar(250)			,
+city			varchar(250)			,
+village			varchar(250)			,
+createdby		varchar(150)			,
+createddate		smalldatetime			,
+modifiedby		varchar(150)			,
 modifieddate	smalldatetime
+primary key(locationcode)
 )
